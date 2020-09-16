ead72091997c12241e1f751b04a9498a2830f2f5
Jay Pedregosa <alexanderj.pedregosa@gmail.com>
2020-09-16 04:11:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,9 @@
 #write your code here
-number = 10
 
 def countdown(number)
   while number > 0 
     puts "#{number} SECOND(S)!"
     number -= 1
   end
-  "Happy New Year!" 
+  "HAPPY NEW YEAR!" 
 end
